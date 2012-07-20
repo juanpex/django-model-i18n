28e2bc236ef33e5342e7a2d624d95712167a1363
juanpex <jpma55@gmail.com>
2012-07-20 01:33:36 -0300

    cleanup and add TRANSLATION_FILENAMES for multisites configs

--- model_i18n/__init__.py
+++ model_i18n/__init__.py
@@ -2,12 +2,9 @@
 import inspect
 from threading import local
 
-from django.conf import settings
-from django.core.exceptions import ImproperlyConfigured
 from django.db.models.manager import signals
 
 import patches
-from utils import import_module
 
 
 VERSION = (0, 4, 0, 'alpha', 0)
@@ -33,33 +30,6 @@ def get_do_autotrans():
 
 def set_do_autotrans(v):
     _active.value = v
-
-
-def _load_conf(*args, **kwargs):
-    """
-    Ensures the configuration module gets
-    imported when importing model_i18n.
-    """
-    # This is an idea from haystack app. We need to run the code that
-    # follows only once, no matter how many times the main module is imported.
-    # We'll look through the stack to see if we appear anywhere and simply
-    # return if we do, allowing the original call to finish.
-    stack = inspect.stack()
-    for stack_info in stack[1:]:
-        if '_load_conf' in stack_info[3]:
-            return
-
-    if not hasattr(settings, 'MODEL_I18N_CONF'):
-        raise ImproperlyConfigured('You must define the MODEL_I18N_CONF \
-        setting, it should be a python module path string, \
-        for example "myproject.i18n_conf"')
-    if not hasattr(settings, 'MODEL_I18N_MASTER_LANGUAGE'):
-        raise ImproperlyConfigured('You must define the \
-        MODEL_I18N_MASTER_LANGUAGE setting.')
-
-    if settings.MODEL_I18N_CONF and settings.USE_I18N:
-        # Import config module
-        import_module(settings.MODEL_I18N_CONF)
 
 
 def ensure_models(**kwargs):
