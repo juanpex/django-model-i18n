0909a41de1938d5436ff64cb03c349367f4aa2e3
juanpex <jpma55@gmail.com>
2013-06-28 13:29:10 -0700

    Update urls.py
    fix for 1.6

--- model_i18n/urls.py
+++ model_i18n/urls.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls.defaults import patterns, url
+except ImportError:
+    from django.conf.urls import patterns, url
+
 
 urlpatterns = patterns('',
     url(r'^$', 'model_i18n.views.model_i18n_set_language', name='setlang'),
