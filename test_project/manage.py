e0b0e2e65bc7192e8aec1d1bc03776834a3169a0
juanpex <jpma55@gmail.com>
2013-09-09 03:42:06 -0300

    travis error import settings

--- test_project/manage.py
+++ test_project/manage.py
@@ -1,24 +1,14 @@
 #!/usr/bin/env python
-try:
-    import os
-    import sys
-    from django.core.management import execute_manager
-except:
-    execute_manager = None
+import os
+import sys
 
-try:
-    import settings # Assumed to be in the same directory.
-except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
-    sys.exit(1)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_project.settings")
 
 if __name__ == "__main__":
-    if execute_manager:
+    try:
+        from django.core.management import execute_manager
+        import settings
         execute_manager(settings)
-    else:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_project.settings")
-
+    except ImportError:
         from django.core.management import execute_from_command_line
-
         execute_from_command_line(sys.argv)
